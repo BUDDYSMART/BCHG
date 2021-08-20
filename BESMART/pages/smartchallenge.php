eaf9c3ecc89285664df772c42a3ff0a9ba734910
BUDDYSMART <benchachindaofficial@gmail.com>
2021-08-20 08:11:17 +0700

    Create smartchallenge.php

---
+++ BESMART/pages/smartchallenge.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8"
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>BCHG : BE SMART</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/buddy-favicon.png" rel="icon">
+  <link href="../assets/img/buddy-touch.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: OnePage - v4.3.0
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+    .buddy-bg {
+	  width: 100%;
+	  height: 100vh;
+	  position: relative;
+	  background-image: url("assets/img/hero-bg2.jpg") top center;
+	  background-size: cover;
+	  position: relative;
+	}
+  </style>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><img src="../img/BUDDY-B.png"> <a href="index.html">BE SMART</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <!--<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto o" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#covid">COVID-19</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          <!--<li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>-->
+        </ul>
+        <!--<i class="bi bi-list mobile-nav-toggle"></i>-->
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+      <div class="row justify-content-center">
+        <div class="col-xl-7 col-lg-9 text-center ">
+		  <!--<h1>BCHG</h1>
+          <img src="../img/SMART.png" width="80%">
+          <h2>Challenge</h2>-->
+		  <iframe width="100%" height="315" src="https://www.youtube.com/embed/HE-Cd-8_nqM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+	  <!--<div class="text-center">
+        <a href="#about" class="btn-get-started scrollto">คลิก</a>
+      </div>-->
+	  <div class="text-center">
+      </div>
+
+      <!--<div class="row icon-boxes">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box" style="background-image: url('img/COVID_box.jpg');top center;background-size: cover;position: relative; ">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <h4 class="title"><a href="">สถานการณ์ BCHG COVID-19</a></h4>
+            <p class="description">
+			<h5 class="text-danger">เคสผู้ป่วยยืนยัน</small></h5>
+			<span data-purecounter-start="0" data-purecounter-end="14" data-purecounter-duration="1" class="purecounter text-danger h1"><small>ราย</small></span><br>
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+			
+			</p>
+			<div class="footer">
+			  <span class="text-right">อ่านเพิ่มเติม</span>
+			</div>
+          </div>
+		  
+        </div>
+
+       
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="500">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-fingerprint-line"></i></div>
+            <h4 class="title"><a href="">Nemo Enim</a></h4>
+            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+          </div>
+        </div>
+
+      </div>
+    </div>-->
+  </section><!-- End Hero -->
+ 
+  <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container" data-aos="fade-up">
+	   
+		<div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
+		 <h2>ของรางวัลแลก B-Point</h2><span class="text-right"><a href="reward.php" class="glightbox play-btn mb-4" data-vbtype="image" data-autoplay="true">ดูทั้งหมด</a></span>
+          <div class="swiper-wrapper">
+
+            
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/2.png" alt="" width="100%">
+                <h3>ลําโพงบลูทูธ</h3>
+                <!--<h4>Ceo &amp; Founder</h4>-->
+              </div>
+            </div><!-- End testimonial item -->
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/3.png" alt="" width="100%">
+                <h3>MI stick tv</h3>
+              </div>
+            </div><!-- End testimonial item -->
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/4.png" alt="" width="100%">
+                <h3>นาฬิกาสมาร์ทวอทช์</h3>
+              </div>
+            </div><!-- End testimonial item -->
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/5.png" alt="" width="100%">
+                <h3>ข้าวสาร RBK</h3>
+              </div>
+            </div><!-- End testimonial item -->
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/6.png" alt="" width="100%">
+                <h3>สมุด BCH</h3>
+              </div>
+            </div><!-- End testimonial item -->
+			
+			<div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="../img/pages/1.png" alt="" width="100%">
+                <h3>ที่ปิดตา  BCH</h3>
+              </div>
+            </div><!-- End testimonial item -->
+
+            
+
+          </div>
+		  <h5 class="text-danger text-center">*ของรางวัลมีจำนวนจำกัด สามารถใช้สิทธิได้เดือนละ 1 ครั้งเท่านั้น</h5>
+		  <span><center><small>
+		    <a href="../img/pages/point.jpg" class="glightbox play-btn mb-4" data-vbtype="image" data-autoplay="true">ดูวิธีการแลกของรางวัล คลิก</a>
+		  </small></center></span>
+          <div class="swiper-pagination"></div>
+		 
+        </div>
+		
+		
+        <div class="section-title">
+          <h2>B-Point Ranking</h2>
+          <!--<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>-->
+		  <div class="row">
+			<div class="col-12">
+			  <input class="form-control" id="myInput" type="text" placeholder="Search.....">
+			</div>
+		  </div>
+		  <div class="row">
+          <div class=" col-12">
+            <div class="py-2">
+            </div>  
+            <div class="table-responsive">
+              <table class="table table-striped table-hover table-bordered">
+                <thead>
+                  <tr>
+                    <th class="text-center">No.</th>
+                    <th class="text-center">Point</th>
+					<th class="text-center">ID</th>
+                    <th class="">Name</th>
+                    <th class="text-center">Company</th>
+                  </tr>
+                </thead>
+                <tbody  id="myTable">
+                <?
+                $row = 1;
+                  if (($handle = fopen("../data/00alldata.txt", "r")) !== FALSE) {
+                      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                          //$num = count($data);
+						$num = 200;
+                          //echo "<p> $num fields in line $row: <br /></p>\n";
+                          echo '<tr>';
+                              echo "<td scope='col' class='text-center text-nowrap'>";
+						if($data[0]==1){
+						 echo "<img src='../img/arrow1.png' width='25px'>";
+						}elseif($data[0]==0){
+						 echo "<img src='../img/arrow3.png' width='25px'>";
+						}else{
+						 echo "<img src='../img/arrow2.png' width='25px'>";
+						}
+						echo "<strong> $data[1]</strong></td>";
+						echo "<td scope='col' class='text-right text-nowrap'>$data[2] <img src='../img/B-Point.png' width='25px'></td>";
+						echo "<td scope='col' class='text-nowrap'>$data[3]</td>";
+						echo "<td scope='col' class='text-left text-nowrap'>$data[4]</td>";
+						echo "<td scope='col' class='text-center'>$data[5]</td>";
+								 
+						echo '</tr>';
+						$row++;	
+                      }
+                      fclose($handle);
+                  }
+                ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+		  <!--
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Six</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+		  -->
+          </div>
+        </div>
+
+        
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+    
+
+  
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          <small>&copy; Copyright <strong><span>By Corporate People Service</span></strong></small>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://www.facebook.com/groups/benchachinda.society" class="facebook"><i class="bx bxl-facebook"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/aos/aos.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/purecounter/purecounter.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/jquery/jquery.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+<script>
+  $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });x
+    });
+  });
+</script>
+
+
+
+</body>
+
+</html>

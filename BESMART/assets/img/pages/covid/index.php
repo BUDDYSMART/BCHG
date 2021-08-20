206f7723de8ff31dc90f732cb92600814eb7a1ec
BUDDYSMART <benchachindaofficial@gmail.com>
2021-08-20 08:06:02 +0700

    Create index.php

---
+++ BESMART/assets/img/pages/covid/index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>BCHG : BE SMART</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../../assets/img/buddy-favicon.png" rel="icon">
+  <link href="../../assets/img/buddy-touch.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../../assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: OnePage - v4.3.0
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+    .buddy-bg {
+	  width: 100%;
+	  height: 100vh;
+	  position: relative;
+	  background-image: url("assets/img/hero-bg2.jpg") top center;
+	  background-size: cover;
+	  position: relative;
+	}
+  </style>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+      <h1 class="logo"><img src="../../img/BUDDY-B.png"> <a href="index.html">BE SMART</a></h1>
+      <nav id="navbar" class="navbar">
+        <ul>
+        </ul>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+  <br><br>
+  <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts section-bg">
+      <div class="container">
+		<div class="section-title">
+          <h3> สถานการณ์เกี่ยวกับ COVID-19</h3>
+		</div>
+        <div class="row justify-content-end">
+          <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="2" class="purecounter"></span>
+              <p>เคสผู้ป่วยยืนยัน</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <span data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="2" class="purecounter"></span>
+              <p>สำนักงานใหญ่</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <span data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="2" class="purecounter"></span>
+              <p>คลังสินค้า</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <span data-purecounter-start="0" data-purecounter-end="3" data-purecounter-duration="2" class="purecounter"></span>
+              <p>ศูนย์ต่างจังหวัด</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="section-title">
+	      <a data-bs-toggle="collapse" data-bs-target="#faq-list-aa" class="collapsed"><i class="bx bx-help-circle icon-help"></i> ดูข้อมูลเพิ่มเติม </a>
+			<div id="faq-list-aa" class="collapse" data-bs-parent=".faq-list">
+			  <img src="img/case28.jpg" width="100%"><br>
+			  <!--<p><a href="https://drive.google.com/file/d/1CFd6K1tI9g6GHaWe9W5CJvN-3G4zDcv5/view">16 สิงหาคา 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://drive.google.com/file/d/1xnIZYeJzfsk7x6N9EazCpE9A-Nc2zUcD/view">13 สิงหาคา 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://drive.google.com/file/d/1EKTdx08bTEkZK_2cQADDUImC3zd0FG0l/view">09 สิงหาคา 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://www.facebook.com/BenchachindaHolding/photos/gm.3117308511830314/1951244938378399/">06 สิงหาคา 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://www.facebook.com/BenchachindaHolding/photos/gm.3114203695474129/1948162022020024/">02 สิงหาคา 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://www.facebook.com/BenchachindaHolding/photos/pcb.3112058635688635/1945976778905215/">30 กรกฎาคม 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>
+			  <p><a href="https://www.facebook.com/BenchachindaHolding/photos/pcb.3112058635688635/1945976778905215/">29 กรกฎาคม 2564 : ประกาศ - ชี้แจงสถานการณ์ COVID-19</a></p>-->
+			</div>
+		</div>
+		
+		
+		
+              
+        
+		
+		
+      </div>
+	  
+    </section><!-- End Counts Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <section id="faq" class="faq section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h3> มาตรการเกี่ยวกับโควิด</h3>
+		</div>
+
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a href="https://drive.google.com/file/d/1skduJCIdWnxPLMaFqaM_Gvulyt9QUezj/view">ระเบียบปฎิบัติในการเข้าปฏิบัติงานในสำนักงานกลุ่มบริษัทเบญจจินดา (22 ก.ค. 2564)</a>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="100">
+              <i class="bx bx-help-circle icon-help"></i> <a href="https://drive.google.com/file/d/1Uf7PddeKtA8f_xqkxVF0it5JGyeJ03pd/view">เรื่อง - มาตรการเฝ้าระวัง ป้องกัน และควบคุมโรคโควิด 19 (เพิ่มเติม)</a>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="200">
+              <i class="bx bx-help-circle icon-help"></i> <a href="http://bensavings.com/SMART/pr/014-2564.pdf">เรื่อง มาตรการป้องกันการแพร่ระบาดไวรัสโคโรน่าสายพันธุ์ใหม่ 2019 (COVID-19) ระลอกใหม่ (เพิ่มเติม)</a>
+            </li>
+
+
+          </ul>
+        </div>
+
+      </div>
+    </section><!-- End Frequently Asked Questions Section -->
+
+	
+	
+	 <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>ประชาสัมพันธ์</h2>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="150">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">ทั้งหมด</li>
+              <li data-filter=".filter-app">ประเภทความเสี่ยง</li>
+              <li data-filter=".filter-card">มาตรการการป้องกันจาก COVID-19</li>
+			  <li data-filter=".filter-info">แบบรายงาน / แบบสำรวจ</li>
+              <li data-filter=".filter-web">เบอร์ติดต่อ</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="300">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/covid.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>ประเภทความเสี่ยง</h4>
+                <p>ระดับความเสี่ยง COVID-19 ตามมาตรการ BCHG</p>
+                <div class="portfolio-links">
+                  <a href="../../img/Covid05.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="ประเภทความเสี่ยง"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/contact.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>เบอร์ติดต่อฝ่ายทรัพยากรบุคคล</h4>
+                <div class="portfolio-links">
+                  <a href="img/contact.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="เบอร์ติดต่อฝ่ายทรัพยากรบุคคล"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/AD1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>การป้องกันจาก COVID-19</h4>
+                <div class="portfolio-links">
+                  <a href="img/AD1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="การป้องกันจาก COVID-19"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-info">
+            <div class="portfolio-wrap">
+              <img src="img/survey01.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>รายงายความเสี่ยง</h4>
+                <div class="portfolio-links">
+                  <a href="img/survey01.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="รายงายความเสี่ยง"><i class="bx bx-plus"></i></a>
+				  <a href="https://docs.google.com/forms/d/e/1FAIpQLSeQokyknCumDpXqamQRS1FWx3gDOZV-jnD-o4ROtZCDQvGftg/viewform" title="More Details"><i class="bx bx-link"></i></a>
+                </div>
+				
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-info">
+            <div class="portfolio-wrap">
+              <img src="img/vaccine.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>แจ้งสถานะการรับวัคซีน</h4>
+                <div class="portfolio-links">
+                  <a href="img/vaccine.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="แจ้งสถานะการรับวัคซีน"><i class="bx bx-plus"></i></a>
+				  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfapREVmQQs5vJzLwjNgb431egnh6sMP4rb2QefOlW_lIj1YQ/viewform" title="More Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-info">
+            <div class="portfolio-wrap">
+              <img src="img/home.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>แบบรายงานสุขาพในช่วงกักตัว</h4>
+                <div class="portfolio-links">
+                  <a href="img/home.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="แบบรายงานสุขาพในช่วงกักตัว"><i class="bx bx-plus"></i></a>
+				  <a href="https://docs.google.com/forms/d/e/1FAIpQLSd9N-eqwHin3Esf7sEY2F9yqdP0TRAGXOGEgIsaonr4X4W43g/viewform" title="More Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/canteen.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>รอบการใช้ห้องอาหาร</h4>
+                <div class="portfolio-links">
+                  <a href="mg/canteen.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-info">
+            <div class="portfolio-wrap">
+              <img src="img/provice.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>การเดินทางข้ามจังหวัด</h4>
+                <div class="portfolio-links">
+                  <a href="img/provice.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="การเดินทางข้ามจังหวัด"><i class="bx bx-plus"></i></a>
+				  <a href="https://docs.google.com/forms/d/e/1FAIpQLSd9N-eqwHin3Esf7sEY2F9yqdP0TRAGXOGEgIsaonr4X4W43g/viewform" title="More Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+
+
+
+
+ 
+ 
+
+    
+
+  
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          <small>&copy; Copyright <strong><span>By Corporate People Service</span></strong></small>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://www.facebook.com/groups/benchachinda.society" class="facebook"><i class="bx bxl-facebook"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../../assets/vendor/aos/aos.js"></script>
+  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../../assets/vendor/purecounter/purecounter.js"></script>
+  <script src="../../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../../assets/vendor/jquery/jquery.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../../assets/js/main.js"></script>
+<script>
+  $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });x
+    });
+  });
+</script>
+
+
+
+</body>
+
+</html>
